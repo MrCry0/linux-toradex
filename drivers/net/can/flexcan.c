72f67cd5a1ef02e23faecdbe068c2711b8e6f7f9
Philippe Schenker <philippe.schenker@toradex.com>
2019-02-01 16:00:13 +0100

    net: can: flexcan: Add pinmuxing in runtime_supsend
    Due to the nature of CAN Tx/Rx pins have to be high when they are
    idle. After boot, CAN is put into runtime_suspend state. When using
    xceiver-supply in DT the power to the transceiver is turned off in this
    state. The Tx/Rx pins are then causing backfeeding.
    
    This patch adds selection of the sleep-state pins during
    runtime_suspend, so someone is able to define another muxing
    in this sate.
    
    Signed-off-by: Philippe Schenker <philippe.schenker@toradex.com>
    (cherry picked from commit daeb17663b0ea58e49dadb404e43e9e22a5b0ec3)
    
    Conflicts:
    	drivers/net/can/flexcan.c
    Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

--- drivers/net/can/flexcan.c
+++ drivers/net/can/flexcan.c
@@ -2185,6 +2185,7 @@ static int __maybe_unused flexcan_runtime_suspend(struct device *device)
 	struct flexcan_priv *priv = netdev_priv(dev);
 
 	flexcan_clks_disable(priv);
+	pinctrl_pm_select_sleep_state(priv->dev);
 
 	return 0;
 }
@@ -2194,6 +2195,7 @@ static int __maybe_unused flexcan_runtime_resume(struct device *device)
 	struct net_device *dev = dev_get_drvdata(device);
 	struct flexcan_priv *priv = netdev_priv(dev);
 
+	pinctrl_pm_select_default_state(priv->dev);
 	return flexcan_clks_enable(priv);
 }
 
