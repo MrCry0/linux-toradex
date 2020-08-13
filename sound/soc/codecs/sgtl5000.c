foundriesio/linux

ASoC: sgtl5000: Restore mclk on resume device
sgtl5000 goes to fail state after suspend system:
[   60.308603] sgtl5000 16-000a: ASoC: Failed to turn on bias: -5
[   60.309264] sgtl5000 16-000a: Unable to sync registers 0x2-0x6. -5

To fix it, mclk is forced to reenable on system resume.

Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@toradex.com>

sound/soc/codecs/sgtl5000.c
@@ -1437,9 +1437,35 @@ static int sgtl5000_remove(struct snd_soc_codec *codec)
 	return 0;
 }
 
+static int sgtl5000_suspend(struct snd_soc_codec *codec)
+{
+	struct sgtl5000_priv *sgtl5000 = snd_soc_codec_get_drvdata(codec);
+
+	clk_disable_unprepare(sgtl5000->mclk);
+
+	return 0;
+}
+
+static int sgtl5000_resume(struct snd_soc_codec *codec)
+{
+	int ret;
+	struct sgtl5000_priv *sgtl5000 = snd_soc_codec_get_drvdata(codec);
+
+	ret = clk_prepare_enable(sgtl5000->mclk);
+	if (ret)
+		dev_err(codec->dev, "Error enabling clock %d\n", ret);
+
+	/* Need 8 clocks before I2C accesses */
+	udelay(1);
+
+	return ret;
+}
+
 static const struct snd_soc_codec_driver sgtl5000_driver = {
 	.probe = sgtl5000_probe,
 	.remove = sgtl5000_remove,
+	.suspend = sgtl5000_suspend,
+	.resume = sgtl5000_resume,
 	.set_bias_level = sgtl5000_set_bias_level,
 	.suspend_bias_off = true,
 	.component_driver = {

MrCry0/linux-toradex

(cherry picked from commit 475eb701f1a774b713433dfdfbc52fd04e8e6691)
Conflicts:
	sound/soc/codecs/sgtl5000.c
Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

sound/soc/codecs/sgtl5000.c
@@ -1514,8 +1514,34 @@ err:
 	return ret;
 }
 
+static int sgtl5000_suspend(struct snd_soc_component *component)
+{
+	struct sgtl5000_priv *sgtl5000 = snd_soc_component_get_drvdata(component);
+
+	clk_disable_unprepare(sgtl5000->mclk);
+
+	return 0;
+}
+
+static int sgtl5000_resume(struct snd_soc_component *component)
+{
+	int ret;
+	struct sgtl5000_priv *sgtl5000 = snd_soc_component_get_drvdata(component);
+
+	ret = clk_prepare_enable(sgtl5000->mclk);
+	if (ret)
+		dev_err(component->dev, "Error enabling clock %d\n", ret);
+
+	/* Need 8 clocks before I2C accesses */
+	udelay(1);
+
+	return ret;
+}
+
 static const struct snd_soc_component_driver sgtl5000_driver = {
 	.probe			= sgtl5000_probe,
+	.suspend		= sgtl5000_suspend,
+	.resume			= sgtl5000_resume,
 	.set_bias_level		= sgtl5000_set_bias_level,
 	.controls		= sgtl5000_snd_controls,
 	.num_controls		= ARRAY_SIZE(sgtl5000_snd_controls),